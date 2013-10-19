ef616271811e18bd01307ce7eef1f6f232774bc0
Andrew Sutton <met48@met48.com>
2013-10-19 23:09:03 +0000

    Init.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='inibin',
+    packages=['inibin'],
+    version='0.1.0',
+    url='http://github.com/Met48/inibin',
+    license='MIT',
+    author='Andrew Sutton',
+    author_email='me@andrewcsutton.com',
+    description='A pure python library for reading League of Legends inibin files.',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
